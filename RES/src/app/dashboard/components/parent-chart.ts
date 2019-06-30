e2e49302fb3f4cf19124cb4f1da3bebde15170bf
MohammedAl-rowad <rowadwo@gmail.com>
2019-06-30 17:35:53 +0300

    adding // TODO, so in the future I add a logic for setExpanded in case of object

--- RES/src/app/dashboard/components/parent-chart.ts
+++ RES/src/app/dashboard/components/parent-chart.ts
@@ -1,12 +1,15 @@
 import { EventEmitter } from '@angular/core';
-import { ComponentDashboardConfigs } from 'src/configs/generalConfig.interface';
+import {
+  ComponentDashboardConfigs,
+  MergedSelect,
+} from 'src/configs/generalConfig.interface';
 import { ChartMathodsService } from './services/chartCommonMethods/chart-mathods.service';
 import { Bucket } from 'src/app/filters/services/interfaces';
 import { ParentComponent } from 'src/app/parent-component.class';
 
 export class ParentChart extends ParentComponent {
   chartOptions: Highcharts.Options;
-  protected buildOptions: EventEmitter<Array<Bucket>>;
+  protected buildOptions: EventEmitter<Array<Bucket> | MergedSelect>;
   constructor(public readonly cms: ChartMathodsService) {
     super();
     this.buildOptions = new EventEmitter<Array<Bucket>>();
@@ -15,9 +18,14 @@ export class ParentChart extends ParentComponent {
 
   protected init(type: string, cb?: () => any) {
     this.cms.init(type, this.componentConfigs as ComponentDashboardConfigs, cb);
-    this.cms.goBuildDataSeries.subscribe((bu: Bucket[]) => {
+    this.cms.goBuildDataSeries.subscribe((bu: Bucket[] | MergedSelect) => {
       if (bu) {
-        this.cms.setExpanded = bu.length >= 1;
+        if (Array.isArray(bu)) {
+          this.cms.setExpanded = bu.length >= 1;
+        } else {
+          //   TODO :::::
+          this.cms.setExpanded = true;
+        }
       } else {
         this.cms.setExpanded = false;
       }
