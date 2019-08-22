38ca72aeb74a07557a3ffd61acb4088882e1749e
moayadnajd <moayad.najd@gmail.com>
2019-08-22 14:42:08 +0300

    update

--- backend/harvest/test.ts
+++ backend/harvest/test.ts
@@ -2,46 +2,51 @@ import { AddOn } from "./src/plugins/Dspace/addOn/AddOn";
 import { Altmetric, DownloadsAndViewsMEL, DownloadsAndViews } from "./src/plugins/Dspace";
 
 var Que = new AddOn();
+Que.clean().then(d => {
 
-var addOnObj10947 = new Altmetric();
+    var addOnObj10947 = new Altmetric();
 
-addOnObj10947.process();
-
-
-addOnObj10947.init("10947")
-
-var addOnObj10568 = new Altmetric();
-
-addOnObj10568.process();
+    addOnObj10947.process();
 
 
-addOnObj10568.init("10568")
+    addOnObj10947.init("10947")
 
-var addOnObjMEL = new Altmetric();
+    var addOnObj10568 = new Altmetric();
 
-addOnObjMEL.process();
-
-
-addOnObjMEL.init("20.500.11766")
-
-var downjMEL = new DownloadsAndViewsMEL();
-downjMEL.process();
-downjMEL.init();
-
-var down = new DownloadsAndViews();
-down.process();
-down.init('https://cgspace.cgiar.org');
+    addOnObj10568.process();
 
 
+    addOnObj10568.init("10568")
 
-let timeout: any = null;
+    var addOnObjMEL = new Altmetric();
 
-Que.queue.on('global:drained', () => {
-    if (timeout) {
-        clearTimeout(timeout);
-        console.log("time cleared")
-    }
-    timeout = setTimeout(() => {
-        console.log("global:drained AddOn");
-    }, 60000);
+    addOnObjMEL.process();
+
+
+    addOnObjMEL.init("20.500.11766")
+
+    var downjMEL = new DownloadsAndViewsMEL();
+    downjMEL.process();
+    downjMEL.init();
+
+    var down = new DownloadsAndViews();
+    down.process();
+    down.init('https://cgspace.cgiar.org');
+
+
+
+    let timeout: any = null;
+
+    Que.queue.on('global:drained', () => {
+        if (timeout) {
+            clearTimeout(timeout);
+            console.log("time cleared")
+        }
+        timeout = setTimeout(() => {
+            console.log("global:drained AddOn");
+        }, 60000);
+    });
+
+
+
 });
