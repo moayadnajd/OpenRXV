947615d08ae3ecc175e490fd0ff99c58ba7a3f46
moayadnajd <moayad.najd@gmail.com>
2019-09-01 16:04:58 +0300

    test

--- backend/harvest/test.ts
+++ backend/harvest/test.ts
@@ -1,7 +1,7 @@
 import { fixLanguage, removeGlobal, removeSponsorship } from "./src/plugins/Dspace";
 
-//fixLanguage().then(d => console.log(d)).catch(e => console.dir(e));
+fixLanguage().then(d => console.log(d)).catch(e => console.dir(e));
 
 removeGlobal().then(d => console.log(d)).catch(e => console.dir(e));
 
-//removeSponsorship().then(d => console.log(d)).catch(e => console.dir(e));
+removeSponsorship().then(d => console.log(d)).catch(e => console.dir(e));
