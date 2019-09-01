c39952d5bd6f0e843d1c21fc2cf9085f178ba6d0
moayadnajd <moayad.najd@gmail.com>
2019-09-01 15:59:56 +0300

    test

--- backend/harvest/test.ts
+++ backend/harvest/test.ts
@@ -2,6 +2,6 @@ import { fixLanguage, removeGlobal, removeSponsorship } from "./src/plugins/Dspa
 
 //fixLanguage().then(d => console.log(d)).catch(e => console.dir(e));
 
-//removeGlobal().then(d => console.log(d)).catch(e => console.dir(e));
+removeGlobal().then(d => console.log(d)).catch(e => console.dir(e));
 
 //removeSponsorship().then(d => console.log(d)).catch(e => console.dir(e));
