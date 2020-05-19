d66d2111aca2583d96963dc6e0f4cf0ab9b4ef18
moayadnajd <moayad.najd@gmail.com>
2020-05-19 23:13:40 +0300

    test > /dev/null

--- docker/docker-setup.sh
+++ docker/docker-setup.sh
@@ -2,7 +2,7 @@
 
 set +e
 
-sudo apt-get -y update 
+sudo apt-get -y update > /dev/null
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -10,11 +10,11 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get -y update
+sudo apt-get -y update > /dev/null
 
-sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io > /dev/null
 
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER > /dev/null
 
 newgrp docker 
 
@@ -24,8 +24,8 @@ sudo echo "vm.max_map_count=262144" >> /etc/sysctl.conf
 
 sysctl -w vm.max_map_count=262144
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose > /dev/null
 
 sudo docker run hello-world
