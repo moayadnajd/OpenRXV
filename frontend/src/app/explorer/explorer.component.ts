2795052ca08a3743f3bc50571aef87fa285d9de7
Abdullah_Mahmoud <Abdullah.foqha1@gmail.com>
2020-07-19 14:47:59 +0300

    colors

--- frontend/src/app/explorer/explorer.component.ts
+++ frontend/src/app/explorer/explorer.component.ts
@@ -88,7 +88,10 @@ export class ExplorerComponent implements OnInit {
     if (appearance.logo)
       this.logo = environment.api + '/' + appearance.logo;
     this.website_name = appearance.website_name;
-
+    localStorage.setItem('primaryColor', appearance.primary_color)
+    localStorage.setItem('minColor', this.hexToHSL(localStorage.getItem('primaryColor'), "min"))
+    localStorage.setItem('midColor', this.hexToHSL(localStorage.getItem('primaryColor'), "mid"))
+    localStorage.setItem("colors", JSON.stringify(appearance.chartColors));
     this.currenRoute = Object.getOwnPropertyDescriptors(this.currentRouter)
     this.currentUrl = this.currenRoute.location.value._platformLocation.location.href
     this.currentUrl = this.currentUrl.slice(0, this.currentUrl.length - 1)
@@ -118,6 +121,53 @@ export class ExplorerComponent implements OnInit {
     this.dashboardConfig = dashboard.flat(1);
     this.tourConfig = [welcome];
     this.loading$ = this.store.select(fromStore.getLoadingStatus);
+  }
+  hexToHSL(H, term) {
+    // Convert hex to RGB first
+    let r: any = 0, g: any = 0, b: any = 0;
+    if (H.length == 4) {
+      r = "0x" + H[1] + H[1];
+      g = "0x" + H[2] + H[2];
+      b = "0x" + H[3] + H[3];
+    } else if (H.length == 7) {
+      r = "0x" + H[1] + H[2];
+      g = "0x" + H[3] + H[4];
+      b = "0x" + H[5] + H[6];
+    }
+    // Then to HSL
+    r /= 255;
+    g /= 255;
+    b /= 255;
+    let cmin = Math.min(r, g, b),
+      cmax = Math.max(r, g, b),
+      delta = cmax - cmin,
+      h = 0,
+      s = 0,
+      l = 0;
+
+    if (delta == 0)
+      h = 0;
+    else if (cmax == r)
+      h = ((g - b) / delta) % 6;
+    else if (cmax == g)
+      h = (b - r) / delta + 2;
+    else
+      h = (r - g) / delta + 4;
+
+    h = Math.round(h * 60);
+
+    if (h < 0)
+      h += 360;
+
+    l = (cmax + cmin) / 2;
+    s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+    s = +(s * 100).toFixed(1);
+    if (term == 'min')
+      l = +(l * 310).toFixed(1);
+    else
+      l = + (l * 155).toFixed(1)
+
+    return "hsl(" + h + "," + s + "%," + l + "%)";
   }
 
   onMouseMove(event: MouseEvent): void {
